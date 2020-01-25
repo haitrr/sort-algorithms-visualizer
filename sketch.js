ccb49f55d35075e456fc96c06c74e822b3d5744b
vroyibg <vroyibg@gmail.com>
2020-01-25 09:15:25 +0700

    generate and draw sample data

--- sketch.js
+++ sketch.js
@@ -1,15 +1,30 @@
 let sel;
 let algorithm;
+let slider;
+let sample;
+let sampleSize = 200;
+let canvasWidth ;
+let canvasHeigth ;
 
 function setup() {
   // put setup code here
-  createCanvas(windowWidth,windowHeight)
+  canvasWidth = windowWidth
+  canvasHeigth = windowHeight
+  createCanvas(canvasWidth,canvasHeigth)
   createAlgorithmsSelect()
   createSampleSizeSlider()
+  sample = generateSample()
 }
 
 function generateSample() {
-
+  var max = sampleSize
+  let data = []
+  current = 0
+  while(current < max) {
+    data.push(Math.floor(Math.random()*max)+1)
+    current += 1
+  }
+  return data;
 }
 
 function createSampleSizeSlider() {
@@ -37,7 +52,26 @@ function draw() {
   // put drawing code here
   background(255)
   textAlign(CENTER, CENTER)
+  strokeWeight(1)
   text("Sample size", 200,20)
+  if(sampleSize != slider.value()) {
+    sampleSize = slider.value();
+    sample = generateSample();
+  }
+  drawSample();
+}
+
+function drawSample() {
+  let width = canvasWidth/ sampleSize;
+  let y = 50;
+  let x = 30;
+  strokeWeight(width);
+  stroke(0)
+  sample.forEach(value => {
+    let height = canvasHeigth * (value/ sampleSize);
+    line(x,y,x,y+height)
+    x+=width;
+  });
 }
 
 class Algorithm {
