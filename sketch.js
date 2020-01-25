03b4518d44490767d0da468c01374b674cb0415d
vroyibg <vroyibg@gmail.com>
2020-01-25 09:36:05 +0700

    implement selection sort

--- sketch.js
+++ sketch.js
@@ -3,18 +3,19 @@ let algorithm;
 let slider;
 let sample;
 let sampleSize = 200;
-let canvasWidth ;
-let canvasHeigth ;
+let canvasWidth;
+let canvasHeigth;
 
 function setup() {
   // put setup code here
-  canvasWidth = windowWidth
-  canvasHeigth = windowHeight
-  createCanvas(canvasWidth,canvasHeigth)
-  createAlgorithmsSelect()
-  createSampleSizeSlider()
+  canvasWidth = windowWidth;
+  canvasHeigth = windowHeight;
+  createCanvas(canvasWidth, canvasHeigth);
+  createAlgorithmsSelect();
+  createSampleSizeSlider();
   createStartButton();
-  sample = generateSample()
+  algorithm = new SelectionSort();
+  sample = generateSample();
 }
 
 function createStartButton() {
@@ -28,18 +29,19 @@ function onStartButtonPressed() {
 }
 
 function generateSample() {
-  var max = sampleSize
-  let data = []
-  current = 0
-  while(current < max) {
-    data.push(Math.floor(Math.random()*max)+1)
-    current += 1
+  var max = sampleSize;
+  let data = [];
+  current = 0;
+  while (current < max) {
+    data.push(Math.floor(Math.random() * max) + 1);
+    current += 1;
   }
+  createAlgorithm();
   return data;
 }
 
 function createSampleSizeSlider() {
-  slider = createSlider(10, 10000, 200);
+  slider = createSlider(50, 10000, 200);
   slider.position(250, 10);
   slider.style('width', '200px');
 }
@@ -49,46 +51,64 @@ function createAlgorithmsSelect() {
   sel = createSelect();
   sel.position(10, 10);
   sel.option('Selection sort');
-  sel.changed(mySelectEvent);
+  sel.changed(createAlgorithm);
 }
 
-
-function mySelectEvent() {
+function createAlgorithm() {
   let item = sel.value();
-  background(200);
-  text('It is a ' + item + '!', 50, 50);
+  if(item == 'Selection sort') {
+    algorithm = new SelectionSort();
+  }
 }
 
 function draw() {
+  frameRate(30);
   // put drawing code here
-  background(255)
-  textAlign(CENTER, CENTER)
-  strokeWeight(1)
-  text("Sample size", 200,20)
-  if(sampleSize != slider.value()) {
+  background(255);
+  textAlign(CENTER, CENTER);
+  strokeWeight(1);
+  text('Sample size', 200, 20);
+  if (sampleSize != slider.value()) {
     sampleSize = slider.value();
     sample = generateSample();
+  }
+
+  if(!algorithm.isDone()) {
+    algorithm.next();
   }
   drawSample();
 }
 
 function drawSample() {
-  let width = canvasWidth/ sampleSize;
+  let width = canvasWidth / sampleSize;
   let y = 50;
   let x = 0;
   strokeWeight(width);
-  stroke(0)
-  sample.forEach(value => {
-    let height = canvasHeigth * (value/ sampleSize);
-    line(x,y,x,y+height)
-    x+=width;
+  stroke(0);
+  sample.forEach((value) => {
+    let height = canvasHeigth * (value / sampleSize);
+    line(x, y, x, y + height);
+    x += width;
   });
 }
 
-class Algorithm {
+class SelectionSort {
+  current = 0;
 
-}
-
-class SelectionSort extends Algorithm {
-
+  isDone() {
+    return this.current == sample.length;
+  }
+  next() {
+    let min = sample[this.current];
+    let minIndex = this.current;
+    for (let i = this.current; i < sample.length; i++) {
+      if (sample[i] < min) {
+        min = sample[i];
+        minIndex = i;
+      }
+    }
+    sample.splice(minIndex, 1);
+    sample.splice(this.current,0,min);
+    this.current += 1;
+  }
 }
